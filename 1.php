63e5d978a3ff4f15c9a50ec294192abe5a98bf36
dd <alpha53260@gmail.com>
2021-11-12 17:23:09 +0300

    2

--- 1.php
+++ 1.php
@@ -2,6 +2,7 @@
 
 echo '<a href="index.php">главная</a>';
 echo "<h1>Вычисления и условный оператор в PHP</h1>";
+echo "<h3>Простейшая арифметика</h3>";
 
 //1
 echo "<ol><li> Даны два числа. Найти их сумму и произведение.</li>";
@@ -13,8 +14,52 @@ $a = 3;
 $b = 2;
 $c = pow($a, 2) - 2 * $a * $b + pow($b, 2) + 2 * $a * $b;
 echo "3<sup><small>2</small></sup> + 2<sup><small>2</small></sup> 
-   = $c";
+   = $c<br>";
 //3
+
+$y = [1, 5, 4, 2, 2];
+echo '<pre>';
+print_r($y);
+echo '</pre>';
+
+// сортировка выбором
+
+
+$y = [1, 5, 4, 2, 2];
+
+for ($i = 0; $i < count($y) - 1; $i++) {
+    $min = $i;
+    for ($j = $i + 1; $j < count($y); $j++){
+        if ($y[$min] > $y[$j]){
+            $min = $j;
+        }
+    }
+    if ($min != $i) {
+        $buf = $y[$i];
+        $y[$i] = $y[$min];
+        $y[$min] = $buf;
+    }
+}
+echo '<pre>';
+print_r($y);
+echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
