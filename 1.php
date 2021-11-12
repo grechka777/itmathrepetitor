23355db4767875a36a24335d21b028de17bad849
dd <alpha53260@gmail.com>
2021-11-12 17:37:42 +0300

    3

--- 1.php
+++ 1.php
@@ -2,6 +2,27 @@
 
 echo '<a href="index.php">главная</a>';
 echo "<h1>Вычисления и условный оператор в PHP</h1>";
+echo "<h3>сортировка массивов</h3>";
+$y = [1, 5, 4, 2, 2];
+echo '<pre>';
+print_r($y);
+echo '</pre>';
+for ($i = 0; $i < count($y) - 1; $i++) {
+    $min = $i;
+    for ($j = $i + 1; $j < count($y); $j++) {
+        if ($y[$min] > $y[$j]) {
+            $min = $j;
+        }
+    }
+    if ($min != $i) {
+        $buf = $y[$i];
+        $y[$i] = $y[$min];
+        $y[$min] = $buf;
+    }
+}
+echo '<pre>';
+print_r($y);
+echo '</pre>';
 echo "<h3>Простейшая арифметика</h3>";
 
 //1
@@ -17,32 +38,7 @@ echo "3<sup><small>2</small></sup> + 2<sup><small>2</small></sup>
    = $c<br>";
 //3
 
-$y = [1, 5, 4, 2, 2];
-echo '<pre>';
-print_r($y);
-echo '</pre>';
 
-// сортировка выбором
-
-
-$y = [1, 5, 4, 2, 2];
-
-for ($i = 0; $i < count($y) - 1; $i++) {
-    $min = $i;
-    for ($j = $i + 1; $j < count($y); $j++){
-        if ($y[$min] > $y[$j]){
-            $min = $j;
-        }
-    }
-    if ($min != $i) {
-        $buf = $y[$i];
-        $y[$i] = $y[$min];
-        $y[$min] = $buf;
-    }
-}
-echo '<pre>';
-print_r($y);
-echo '</pre>';
 
 
 
